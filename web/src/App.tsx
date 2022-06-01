87bec49bfd8fd670a40f90ce6b2927f27fdc32ff
Mayza Ynara <mayzamrodrigues@gmail.com>
2022-06-01 17:56:01 -0300

    add WidgetForm Feedback

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,5 @@
-interface ButtonProps {
-  text?: string;
+import { Widget } from "./components/Widget";
+
+export function App () {
+  return <Widget/>
 }
-
-
-function Button(props:ButtonProps) {
-  return <button className="bg-violent="> {props.text ?? 'Default' }</button>
-  
-}
-
-
-function App() {
- 
-return (
-     <div>
-      <Button text="Enviar" />
-       <Button text="Ok" />
-       < Button />
-     </div>
-)
-}
-//componente > e uma funcao que retorna html, por exemplo App e uma funcao que retorna html. O componente sempre tem que ter a primeira leite maiuscula tanto do arquivo quanto da funcao. 
-
-
-export default App
